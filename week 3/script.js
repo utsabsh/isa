70213720606514e7338a9e560b38f55290af277f
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2023-03-22 18:56:37 +0545

    week 3 assignment

---
+++ week 3/script.js
@@ -0,0 +1,43 @@
+// var A = 10;
+// let a = 5 + 10;
+// console.log(a);
+// console.log(A);
+// const b = 11;
+// alert(b);
+// document.write("<br>" + "<p>hello world</p>")
+
+// function avg(a,b,c){
+//     return (a+b+c)/3
+// }
+// var team_a = avg(50,75,90);
+// console.log(""team_a);
+// var team_b = avg(65,70,80);
+// console.log(team_b);
+
+// var sum = (e,f) =>{
+//     return e+f;
+// }
+// var c = sum(5,6);
+// console.log(c);
+
+// setTimeout(function(){
+
+//     console.log("hello")
+//     });
+//     var fruits=["Ktm","Bhkt","lalit "];
+//     fruits.forEach(function (fruit){
+//     console.log(fruit);
+//     });
+
+// let country ='Nepal';
+// let continent = 'Asia';
+// let population = 90;
+// console.log("name of my country is :"+country+"which lies in "+continent+" continent and it has popolation of "+population+" million")
+// console.log(`Name of my country is ${country} which lies in ${continent} and it has population of ${population}`)
+
+// var numbers = [65,44,12,4];
+// var newarray = numbers.map(myFunction)
+// function myFunction(num){
+//     return num * 10;
+// }
+// console.log(newarray);
